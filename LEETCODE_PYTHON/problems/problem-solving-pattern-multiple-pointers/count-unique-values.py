a330796a6f935265d6de78d7f1e60f640c33e3c8
Moghany <m.osama.elmoghany@gmail.com>
2024-03-27 12:45:13 +0200

    multiple pointers pattern count unique values

--- LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/count-unique-values.py
+++ LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/count-unique-values.py
@@ -31,30 +31,39 @@ def countUniqueValues(arr):
     #         return unique_count
 
 # 3rd solution
-    if not (arr):
-        return 0
+    # if not (arr):
+    #     return 0
 
-    pointer = 0
-    unique_arr = []
-    unique_pointer = 0
-    while pointer <= len(arr)-1:
-        if pointer == 0:
-            unique_arr.append(arr[pointer])
-            pointer += 1
-        elif pointer < len(arr)-1:
-            if arr[pointer-1] == arr[pointer]:
-                pointer += 1
-            else:
-                unique_arr.append(arr[pointer])
-                pointer += 1
-        else:
-            if arr[pointer-1] == arr[pointer]:
-                return len(unique_arr)
-            else:
-                unique_arr.append(arr[pointer])
-                return len(unique_arr)
+    # pointer = 0
+    # unique_arr = []
+    # unique_pointer = 0
+    # while pointer <= len(arr)-1:
+    #     if pointer == 0:
+    #         unique_arr.append(arr[pointer])
+    #         pointer += 1
+    #     elif pointer < len(arr)-1:
+    #         if arr[pointer-1] == arr[pointer]:
+    #             pointer += 1
+    #         else:
+    #             unique_arr.append(arr[pointer])
+    #             pointer += 1
+    #     else:
+    #         if arr[pointer-1] == arr[pointer]:
+    #             return len(unique_arr)
+    #         else:
+    #             unique_arr.append(arr[pointer])
+    #             return len(unique_arr)
 
-print(f"unique count is", countUniqueValues([1,1,1,1,1,2])) #2
+# 4th solution
+    i=0
+    for j in range(len(arr)):
+        if(arr[i]!=arr[j]):
+            i += 1
+            arr[i] = arr[j]
+        print(f'i=',i,'j=',j)
+    return i+1
+
+# print(f"unique count is", countUniqueValues([1,1,1,1,1,2])) #2
 print(f"unique count is",countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) #7
-print(f"unique count is",countUniqueValues([])) #0
-print(f"unique count is",countUniqueValues([-2,-1,-1,0,1])) #4
+# print(f"unique count is",countUniqueValues([])) #0
+# print(f"unique count is",countUniqueValues([-2,-1,-1,0,1])) #4
