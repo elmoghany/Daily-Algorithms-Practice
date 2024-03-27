32a3a413a768629784fcb2fbf58ebcb532552f99
Moghany <m.osama.elmoghany@gmail.com>
2024-03-27 11:23:45 +0200

    multiple pointers pattern

--- LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/sum-zero.py
+++ LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/sum-zero.py
@@ -1,6 +1,19 @@
-def sumZero(arr):
-    pass
+import sys
 
-sumZero([-3,-2,-1,0,1,2,3]) #[-3,3]
-sumZero([-2,0,1,3]) #undefined
-sumZero([1,2,3]) #undefined
+def sumZero(arr):
+    #return the first pair that sum zero
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        sum = arr[left] + arr[right]
+        if sum == 0:
+            return [arr[left], arr[right]]  
+        elif sum > 0:
+            right -= 1
+        else:
+            left += 1      
+
+#takes a sorted array only
+print(sumZero([-3,-2,-1,0,1,2,3])) #[-3,3]
+print(sumZero([-2,0,1,3])) #undefined
+print(sumZero([1,2,3])) #undefined
