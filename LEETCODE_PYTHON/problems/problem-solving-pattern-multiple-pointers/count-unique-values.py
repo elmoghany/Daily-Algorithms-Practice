0cbf60654d6963e6cc7dc34aab1d76039eebb600
Moghany <m.osama.elmoghany@gmail.com>
2024-03-27 12:26:06 +0200

    multiple pointers pattern count unique values

--- LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/count-unique-values.py
+++ LEETCODE_PYTHON/problems/problem-solving-pattern-multiple-pointers/count-unique-values.py
@@ -7,28 +7,52 @@ def countUniqueValues(arr):
     # return len(a)
     
     #long answer!
+    # if not (arr):
+    #     return 0
+    # unique_count = 0
+    # pointer = 0
+    # last_value = None
+    # while pointer <= len(arr)-1:
+    #     if (pointer == 0 and (arr[0] != None)):
+    #         last_value = arr[pointer]
+    #         unique_count += 1
+    #         pointer += 1
+    #     elif pointer < len(arr)-1:
+    #         if last_value == arr[pointer]:
+    #             last_value = arr[pointer]
+    #             pointer += 1
+    #         else:
+    #             last_value = arr[pointer]
+    #             unique_count += 1
+    #             pointer += 1
+    #     else: #last value of the array
+    #         last_value = arr[pointer]
+    #         unique_count += 1
+    #         return unique_count
+
+# 3rd solution
     if not (arr):
         return 0
-    unique_count = 0
+
     pointer = 0
-    last_value = None
+    unique_arr = []
+    unique_pointer = 0
     while pointer <= len(arr)-1:
-        if (pointer == 0 and (arr[0] != None)):
-            last_value = arr[pointer]
-            unique_count += 1
+        if pointer == 0:
+            unique_arr.append(arr[pointer])
             pointer += 1
         elif pointer < len(arr)-1:
-            if last_value == arr[pointer]:
-                last_value = arr[pointer]
+            if arr[pointer-1] == arr[pointer]:
                 pointer += 1
             else:
-                last_value = arr[pointer]
-                unique_count += 1
+                unique_arr.append(arr[pointer])
                 pointer += 1
-        else: #last value of the array
-            last_value = arr[pointer]
-            unique_count += 1
-            return unique_count
+        else:
+            if arr[pointer-1] == arr[pointer]:
+                return len(unique_arr)
+            else:
+                unique_arr.append(arr[pointer])
+                return len(unique_arr)
 
 print(f"unique count is", countUniqueValues([1,1,1,1,1,2])) #2
 print(f"unique count is",countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])) #7
