057a8bf272511ccd2ad1e5febea4ce8c5fe9a063
Moghany <m.osama.elmoghany@gmail.com>
2024-03-29 11:47:50 +0200

    sliding pattern

--- LEETCODE_PYTHON/problems/problem-solving-pattern-sliding-window/max-subarray-max.py
+++ LEETCODE_PYTHON/problems/problem-solving-pattern-sliding-window/max-subarray-max.py
@@ -9,12 +9,12 @@ def maxSubarraySum(arr, num):
         for i in range(num):
             max_sum += arr[i]
         temp_sum = max_sum
-        for i in range(num,len(arr)-1):
+        for i in range(num,len(arr)):
             temp_sum = temp_sum - arr[i-num] + arr[i]
-            
-
-maxSubarraySum([1,2,5,2,8,1,5],2) #10
-maxSubarraySum([1,2,5,2,8,1,5],4) #17
-maxSubarraySum([4,2,1,6],1) #6
-maxSubarraySum([4,2,1,6,2],4) #13
-maxSubarraySum([],4) #None
+            max_sum = max(temp_sum, max_sum)
+    return max_sum
+print(maxSubarraySum([1,2,5,2,8,1,5],2)) #10
+print(maxSubarraySum([1,2,5,2,8,1,5],4)) #17
+print(maxSubarraySum([4,2,1,6],1)) #6
+print(maxSubarraySum([4,2,1,6,2],4)) #13
+print(maxSubarraySum([],4)) #None
