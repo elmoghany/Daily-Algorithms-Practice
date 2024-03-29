209eb984560f3d3bc3e75cc1e165a09533b8fec4
Moghany <m.osama.elmoghany@gmail.com>
2024-03-29 11:17:20 +0200

    sliding pattern

---
+++ LEETCODE_PYTHON/problems/problem-solving-pattern-sliding-window/max-subarray-max.py
@@ -0,0 +1,13 @@
+import sys
+
+def maxSubarraySum(arr, num):
+    if num > len(arr):
+        return null
+    else:
+        pass
+
+maxSubarraySum([1,2,5,2,8,1,5],2)
+maxSubarraySum([1,2,5,2,8,1,5],4)
+maxSubarraySum([4,2,1,6],1)
+maxSubarraySum([4,2,1,6,2],4)
+maxSubarraySum([],4)
