b53b028bcbcc1901fc8cac46a1844c193d10b021
Moghany <m.osama.elmoghany@gmail.com>
2024-03-31 13:26:59 +0200

    adding linkedlist constructor & print_list

---
+++ LEETCODE_PYTHON/problems/linked-list/LinkedList.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    #create node
+    #initialize new node
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+    #create node
+    #add node to the end
+    def append(self, value):
+        pass
+    #create node
+    #add node to the beginning
+    def prepend(self, value):
+        pass
+    #create node
+    #insert node at index
+    def insert(self, index, value):
+        pass
+    ################################
+    ################################
+    def remove(self, index, value):
+        pass
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
