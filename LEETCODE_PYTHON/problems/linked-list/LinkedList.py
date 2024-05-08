c05bcd637c7e59f23bee1bddd69452e2fb661824
Moghany <m.osama.elmoghany@gmail.com>
2024-05-08 15:02:52 +0300

    reverse_between

--- LEETCODE_PYTHON/problems/linked-list/LinkedList.py
+++ LEETCODE_PYTHON/problems/linked-list/LinkedList.py
@@ -296,21 +296,68 @@ def find_kth_from_end(ll, k):
 
 
 linked_list = LinkedList(1)
-linked_list.append(0)
-linked_list.append(1)
- 
-# Convert binary to decimal
-print(linked_list.binary_to_decimal())  # Output: 5
- 
-# Create a linked list for binary number 1101
-linked_list = LinkedList(1)
-linked_list.append(1)
-linked_list.append(0)
-linked_list.append(1)
- 
-# Convert binary to decimal
-print(linked_list.binary_to_decimal())  # Output: 13
+linked_list.append(2)
+linked_list.append(3)
+linked_list.append(4)
+linked_list.append(5)
 
+print("Original linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist within the linked list
+linked_list.reverse_between(2, 4)
+print("Reversed sublist (2, 4): ")
+linked_list.print_list()
+
+# Reverse another sublist within the linked list
+linked_list.reverse_between(0, 4)
+print("Reversed entire linked list: ")
+linked_list.print_list()
+
+# Reverse a sublist of length 1 within the linked list
+linked_list.reverse_between(3, 3)
+print("Reversed sublist of length 1 (3, 3): ")
+linked_list.print_list()
+
+# Reverse an empty linked list
+empty_list = LinkedList(0)
+empty_list.make_empty
+empty_list.reverse_between(0, 0)
+print("Reversed empty linked list: ")
+empty_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Original linked list: 
+    1
+    2
+    3
+    4
+    5
+    Reversed sublist (2, 4): 
+    1
+    2
+    5
+    4
+    3
+    Reversed entire linked list: 
+    3
+    4
+    5
+    2
+    1
+    Reversed sublist of length 1 (3, 3): 
+    3
+    4
+    5
+    2
+    1
+    Reversed empty linked list: 
+    None
+    
+"""
     
 ###########################################
 ###########################################
