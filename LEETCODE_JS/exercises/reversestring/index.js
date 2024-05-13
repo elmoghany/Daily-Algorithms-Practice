1fc85f2001fb36ca2d7379c0a835f42acbc7581c
Moghany <m.osama.elmoghany@gmail.com>
2024-05-13 17:59:55 +0300

    JAVASCRIPT reverseString

--- LEETCODE_JS/exercises/reversestring/index.js
+++ LEETCODE_JS/exercises/reversestring/index.js
@@ -13,8 +13,18 @@
 //     }
 //     return temp.join('')
 // }
+
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
 function reverse(str) {
-    return str.split('').reverse().join('')
+    let reversed = ''
+
+    for(let character of str){
+        reversed = character + reversed
+    }
+    return reversed
 }
 
 module.exports = reverse;
