6eeab1e297c02c579ec4451f3c23f3355aebab88
Moghany <m.osama.elmoghany@gmail.com>
2024-05-14 19:56:34 +0300

    JAVASCRIPT reverseint

--- LEETCODE_JS/exercises/reverseint/index.js
+++ LEETCODE_JS/exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    if(n==0) return 0
+    let int_sign = Math.sign(n)
+    console.log(int_sign)
+    let rev_str = Math.abs(n).toString().split('').reverse().join('')
+    console.log(rev_str)
+    rev_str = parseInt(rev_str) * int_sign
+    return rev_str
+}
 
+reverseInt(12)
 module.exports = reverseInt;
