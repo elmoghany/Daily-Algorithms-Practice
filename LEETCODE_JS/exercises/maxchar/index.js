b34804ba11f6406a33f7db44d7a91b12508026ef
Moghany <m.osama.elmoghany@gmail.com>
2024-05-14 21:14:15 +0300

    JAVASCRIPT maxchar

--- LEETCODE_JS/exercises/maxchar/index.js
+++ LEETCODE_JS/exercises/maxchar/index.js
@@ -7,13 +7,25 @@
 
 function maxChar(characters) {4
     const char_map = {}
+    let prior_char = -1
+    let max = -1
+    let max_char = null
     for (let char of characters) {
         // char_map[char] = char_map[char] + 1 || 1
         if(!char_map[char]) {
-            char_map[char] = 1 
+            char_map[char] = 1
+        } else {
+            char_map[char] += 1
         }
-        char_map[char] += 1
+        if(!max_char) {
+            max_char = char
+            max = char_map[char]
+        } else if(char_map[char] > max) {
+            max_char = char
+            max = char_map[char]
+        }
     }
+    return max_char
 }
 
 module.exports = maxChar;
