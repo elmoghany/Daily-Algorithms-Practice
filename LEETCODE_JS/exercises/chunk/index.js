444e51de47bda5e330b617cf4b5ab3bbac8711ab
Moghany <m.osama.elmoghany@gmail.com>
2024-05-15 09:55:29 +0300

    JAVASCRIPT array chunk

--- LEETCODE_JS/exercises/chunk/index.js
+++ LEETCODE_JS/exercises/chunk/index.js
@@ -12,23 +12,32 @@ function chunk(array, size) {
     let array_chunks = []
     let chunk_cnt = 0
     let sub_array_cnt = 0
-
-    for(let i=0; i<array.length; i++){
-        if(sub_array_cnt == 0){
-            array_chunks[chunk_cnt] = []
-        }
-        
-        array_chunks[chunk_cnt][sub_array_cnt] = array[i]
-        sub_array_cnt++
-
-        if(sub_array_cnt >= size){
-            sub_array_cnt = 0
-            chunk_cnt++
-        }
+    let index = 0 
+    while(index < array.length){
+        array_chunks.push(array.slice(index , index + size))
+        index = index + size
     }
     console.log(array_chunks)
     return array_chunks
 }
+
+
+//     for(let i=0; i<array.length; i++){
+//         if(sub_array_cnt == 0){
+//             array_chunks[chunk_cnt] = []
+//         }
+        
+//         array_chunks[chunk_cnt][sub_array_cnt] = array[i]
+//         sub_array_cnt++
+
+//         if(sub_array_cnt >= size){
+//             sub_array_cnt = 0
+//             chunk_cnt++
+//         }
+//     }
+//     console.log(array_chunks)
+//     return array_chunks
+// }
 
 chunk([1, 2, 3, 4], 2)
 module.exports = chunk;
