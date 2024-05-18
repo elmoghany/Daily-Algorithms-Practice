b1da5dadfb45c86fc732e2d72cf7a9ddbdf15d9a
Moghany <m.osama.elmoghany@gmail.com>
2024-05-18 15:33:45 +0300

    JAVASCRIPT steps for pyramid ###

--- LEETCODE_JS/exercises/pyramid/index.js
+++ LEETCODE_JS/exercises/pyramid/index.js
@@ -6,14 +6,42 @@
 // --- Examples
 //   pyramid(1)
 //       '#'
-//   pyramid(2)
+//   pyramid(2) => n=rows 
 //       ' # '
 //       '###'
 //   pyramid(3)
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4) => 1,3,5,7,9
+// n-count , count, n-count
+// count = 2
+// 3, 1, 3
+// count = 2
+// 2, 2, 2
+// count = 3
+// 1, 3, 1
+//       '   #   ' 3. 1. 3
+//       '  ###  ' 2. 3. 2
+//       ' ##### ' 1. 5. 1
+//       '#######' 0. 7. 0
 
-function pyramid(n) {}
+function pyramid(n) {
+    str = ''
+    count=1
+    odd_cntr = 1
+    while( count <= n ){
+        if(count <= n){
+            str = console.log(' ').repeat(n-count)
+            str = str + console.log('#').repeat(odd_cntr)
+            str = str + console.log(' ').repeat(n-count)
+        } 
+        // else {
+        //     console.log('#').repeat(n-count+1)
+        // }
+        count++
+        odd_cntr+=2
+    }
+}
 
 module.exports = pyramid;
