bc06d02fb743801047debb1afb6b1eb2ab55cf56
Moghany <m.osama.elmoghany@gmail.com>
2024-05-19 12:39:14 +0300

    javascript sprial matrix

--- LEETCODE_JS/exercises/matrix/index.js
+++ LEETCODE_JS/exercises/matrix/index.js
@@ -22,46 +22,65 @@ function matrix(n) {
     //Sub Matrix creation
     for(let i = 0; i < n; i++){
         sub_mat[i] = []
+        // sub_mat.push([])
     }
 
     /////////////////////////////
 
-    //i=0 row
-    for(let j = 0; j < n; j++){
-        sub_mat[0][j] = last_count
-        last_count++
-    }
+    //i
+    let start_row = 0
+    let end_row = n - 1
+    //j
+    let start_col = 0
+    let end_col = n - 1
     
-    //j=n-1 column
-    for(let i = 1; i < n; i++){
-        sub_mat[i][n-1] = last_count
-        last_count++
+    while(start_col <= end_col && start_row <= end_row){
+        //1
+        //i=0 1st row forward
+        for(let j = start_col; j <= end_col; j++){
+            sub_mat[start_row][j] = last_count
+            last_count++
+        }
+        start_row++
+        
+        //2
+        //j=n-1 last column down
+        for(let i = start_row; i <= end_row; i++){
+            sub_mat[i][n-1] = last_count
+            last_count++
+        }
+        end_col--
+
+        //3
+        //i=n-1 last row backwards
+        for(let j = end_col; j >= start_col; j--){
+            sub_mat[n-1][j] = last_count
+            last_count++
+        }
+        end_row--
+        
+        //4
+        //j=0 1st column up
+        for ( let i = end_row; i >= start_row; i--){
+            sub_mat[i][0] = last_count
+            last_count++
+        }
+        start_col++
     }
 
-    //i=n-1 row
-    for(let j = n-2; j >= 0; j--){
-        sub_mat[n-1][j] = last_count
-        last_count++
-    }
+    // //i=1 row
+    // for( let j = 1; j <= n-2; j++){
+    //     sub_mat[1][j] = last_count
+    //     last_count++
+    // }
 
-    //j=0 column
-    for ( let i = n-2; i > 0; i--){
-        sub_mat[i][0] = last_count
-        last_count++
-    }
+    // //i=2 row
+    // for( let j = n-2; j >= 1; j--){
+    //     sub_mat[2][j] = last_count
+    //     last_count++
+    // }
 
-    //i=1 row
-    for( let j = 1; j <= n-2; j++){
-        sub_mat[1][j] = last_count
-        last_count++
-    }
-
-    //i=2 row
-    for( let j = n-2; j >= 1; j--){
-        sub_mat[2][j] = last_count
-        last_count++
-    }
-
+    console.log(sub_mat)
     return sub_mat
 }
 
