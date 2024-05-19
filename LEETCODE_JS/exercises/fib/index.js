f9479776132f8318ec82ad28b565b98d5dc00ddf
Moghany <m.osama.elmoghany@gmail.com>
2024-05-19 21:17:46 +0300

    javascript fib

--- LEETCODE_JS/exercises/fib/index.js
+++ LEETCODE_JS/exercises/fib/index.js
@@ -9,7 +9,12 @@
 //   fib(4) === 3
 
 function fib(n) {
-    
+    let sum = 0
+    if(n == 0) return 0
+    if(n == 1) return 1
+    sum = sum + fib(n-1) + fib(n-2)
+    return sum
 }
 
+fib(2)
 module.exports = fib;
