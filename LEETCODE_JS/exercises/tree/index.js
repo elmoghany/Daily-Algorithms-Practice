74f0d028a311af0482a75b68fde06b0b23b682fa
Moghany <m.osama.elmoghany@gmail.com>
2024-05-21 19:20:59 +0300

    javascript tree depth first

--- LEETCODE_JS/exercises/tree/index.js
+++ LEETCODE_JS/exercises/tree/index.js
@@ -41,7 +41,14 @@ class Tree {
             fn(node)
         }
     }
-    traverseDF(){
+    traverseDF(fn){
+        const arr = [this.root]
+        while(arr.length){
+            const node = arr.shift()
+            arr.unshift(...node.children)
+            
+            fn(node)
+        }
 
     }
 }
